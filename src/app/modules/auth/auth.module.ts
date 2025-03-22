6b88b39347f43771043c5f4e0ef39708514f283b
Clevin <clevinmwilson@gmail.com>
2025-03-22 10:05:27 +0530

    reactive form added for signup

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './authauth-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [LoginComponent, SignupComponent],
-    imports: [CommonModule, AuthRoutingModule, SharedModule],
+    imports: [CommonModule, AuthRoutingModule, SharedModule, ReactiveFormsModule],
     exports: []
 })
 export class AuthModule {}
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
     styleUrl: './signup.component.css'
 })
-export class SignupComponent {}
+export class SignupComponent {
+    form = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+
+    onFormSubmit() {
+        console.log(this.form.value);
+    }
+}
