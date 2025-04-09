945dc770744fbbadc8b68edc35b716b34e6537af
Clevin <clevinmwilson@gmail.com>
2025-04-09 22:19:10 +0530

    job details card completed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { SolidButtonComponent } from './components/solid-button/solid-button.com
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { OutlineButtonComponent } from './components/outline-button/outline-button.component';
 import { IconCardComponent } from './components/icon-card/icon-card.component';
+import { JobInfoCardComponent } from './components/job-info-card/job-info-card.component';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [SolidButtonComponent, NavbarComponent, OutlineButtonComponent, IconCardComponent],
-    exports: [SolidButtonComponent, NavbarComponent, OutlineButtonComponent, IconCardComponent]
+    declarations: [SolidButtonComponent, NavbarComponent, OutlineButtonComponent, IconCardComponent, JobInfoCardComponent],
+    exports: [SolidButtonComponent, NavbarComponent, OutlineButtonComponent, IconCardComponent, JobInfoCardComponent]
 })
 export class SharedModule {}
