21fd2765bf3b26a854938edca324774808616368
Clevin <clevinmwilson@gmail.com>
2025-04-26 18:36:52 +0530

    company page ui added

--- src/app/modules/companies/company.module.ts
+++ src/app/modules/companies/company.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompaniesListingComponent } from './companies-listing/companies-listing.component';
 import { CompanyRoutingRoutingModule } from './company-routing-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
     declarations: [CompaniesListingComponent],
-    imports: [CommonModule, CompanyRoutingRoutingModule]
+    imports: [CommonModule, CompanyRoutingRoutingModule, SharedModule]
 })
 export class CompanyModule {}
