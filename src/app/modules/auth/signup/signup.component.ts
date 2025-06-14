6188e34b5480264aa5b8670481e134e4a1fe4f59
Clevin <clevinmwilson@gmail.com>
2025-06-14 10:44:28 +0530

    store implemented and change to standalone component and singup page feature added

--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -7,12 +7,45 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+    formSubmitted: boolean = false;
+
     form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+        password: new FormControl('', [Validators.required, Validators.minLength(8)])
     });
 
     onFormSubmit() {
-        console.log(this.form.value);
+        this.formSubmitted = true;
+        if (this.form.valid) {
+            const formData = this.form.value;
+            console.log('Form submitted successfully:', formData);
+        } else if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            console.log('Form is invalid. Please correct the errors.');
+        }
+    }
+
+    get emailIsInvalidErrorMessage(): string {
+        const emailControl = this.form.controls.email;
+        if ((emailControl.touched || this.formSubmitted) && emailControl.invalid) {
+            if (this.form.controls.email.hasError('required')) {
+                return 'Email is required';
+            } else if (this.form.controls.email.hasError('email')) {
+                return 'Email must be a valid email address.';
+            }
+        }
+        return '';
+    }
+
+    get passwordIsInvalidErrorMessage(): string {
+        const passwordControl = this.form.controls.password;
+        if ((passwordControl.touched || this.formSubmitted) && passwordControl.invalid) {
+            if (this.form.controls.password.hasError('required')) {
+                return 'Password is required';
+            } else if (this.form.controls.password.hasError('minlength')) {
+                return 'Password must be at least 8 characters long.';
+            }
+        }
+        return '';
     }
 }
